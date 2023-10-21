6f25839132d7a3c992e19647c9a78ececec92294
Chris Waddell <yesitschris@hotmail.com>
2023-10-21 21:40:40 +1100

    Booking App search bar development

--- booking-app/config/loaders.js
+++ booking-app/config/loaders.js
@@ -1,30 +1,54 @@
+const path = require("path")
+
 const JSLoader = [
-	// Loads css files from import statement in js
-	{
-		test: /\.css$/i,
-		use: ["file-loader"],
-	},
-	{
-		test: /\.css$/i,
-		exclude: /node_modules/,
-		use: ["css-loader"],
-	},
-	{
-		test: /\.css$/i,
-		exclude: /node_modules/,
-		use: ["style-loader"],
-	},
-	// {
-	// test: /\.scss$/i,
-	// use: ["style-loader", "css-loader", "sass-loader"],
-	// },
 	{
 		test: /\.js$/i,
-		// exclude: /node_modules/,
+		exclude: /node_modules/,
 		use: {
 			loader: "babel-loader",
 		},
 	},
+	// Loads css files from import statement in js
+	// {
+	// 	test: /\.css$/i,
+	// 	exclude: /node_modules/,
+	// 	use: ["file-loader"],
+	// },
+	// {
+	// 	test: /\.css$/i,
+	// 	exclude: /node_modules/,
+	// 	use: ["css-loader"],
+	// },
+	// {
+	// 	test: /\.css$/i,
+	// 	exclude: /node_modules/,
+	// 	use: ["style-loader"],
+	// },
+	// {
+	// 	test: /\.scss$/i,
+	// 	exclude: /node_modules/,
+	// 	use: ["css-loader", "file-loader"],
+	// },
+
+	// CSS rules
+	{
+		test: /\.css$/,
+		include: path.join(__dirname, "..", "node_modules", "react-date-range"),
+		use: [
+			"style-loader",
+			{
+				loader: "css-loader",
+				options: {
+					modules: false,
+				},
+			},
+		],
+	},
+
+	// {
+	// 	test: /\.scss$/i,
+	// 	use: ["style-loader", "css-loader", "sass-loader"],
+	// },
 ]
 
 module.exports = {
--- booking-app/src/components/Header.js
+++ booking-app/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
 	faBed,
@@ -21,20 +21,33 @@ const currentDate = () => {
 }
 
 export default function Header() {
-	function handleSelect(ranges) {
-		console.log(ranges)
-		// {
-		//   selection: {
-		//     startDate: [native Date Object],
-		//     endDate: [native Date Object],
-		//   }
-		// }
-	}
+	const [datePickerOpened, setDatePickerOpened] = useState(false)
+	const [peopleSelectOpened, setPeopleSelectOpened] = useState(false)
+	const [bookingOptions, setBookingOptions] = useState({
+		adult: 1,
+		children: 0,
+		room: 1,
+	})
 
-	const selectionRange = {
-		startDate: new Date(),
-		endDate: new Date(),
-		key: "selection",
+	const [state, setState] = useState([
+		{
+			startDate: new Date(),
+			endDate: new Date(),
+			key: "selection",
+		},
+	])
+
+	function handleBookingOptions(name, operation) {
+		const res = {
+			...bookingOptions,
+			[name]:
+				operation === "i"
+					? bookingOptions[name] + 1
+					: bookingOptions[name] - 1,
+		}
+		console.log("res")
+		console.log(res)
+		setBookingOptions(res)
 	}
 
 	return (
@@ -80,7 +93,7 @@ export default function Header() {
 				more wiht a free Lamabook account.
 			</p>
 			<div className="navcontainer">
-				<button type="button" className="c-btn">
+				<button type="button" className="signin c-btn">
 					Sign in / Register
 				</button>
 			</div>
@@ -89,7 +102,6 @@ export default function Header() {
 					<div className="inputs o-flex">
 						<div className="headerSearchItem o-flex--1">
 							<FontAwesomeIcon icon={faBed} />
-							{/* <span>sadfasdfsa</span> */}
 							<input
 								type="text"
 								className="o-flex--1"
@@ -98,31 +110,136 @@ export default function Header() {
 						</div>
 						<div className="headerSearchItem o-flex--1">
 							<FontAwesomeIcon icon={faCalendarDays} />
-							{/* <span>sadfasdfsa</span> */}
 							<input
 								type="text"
 								className="o-flex--1"
 								placeholder={`${currentDate()} to ${currentDate()}`}
+								onClick={() =>
+									setDatePickerOpened(!datePickerOpened)
+								}
 							/>
 						</div>
 						<div className="headerSearchItem o-flex--1">
 							<FontAwesomeIcon icon={faPerson} />
-							{/* <span>sadfasdfsa</span> */}
 							<input
 								type="text"
 								className="o-flex--1"
 								placeholder="1 adult - 0 chilren - 1 room"
+								onClick={() =>
+									setPeopleSelectOpened(!peopleSelectOpened)
+								}
 							/>
+							<div className="headerSearchItem__peoplePopup">
+								<div className="o-flex x--between y--center">
+									<span>Adult</span>
+									<div className="count">
+										<button
+											type="button"
+											onClick={() =>
+												handleBookingOptions(
+													"adult",
+													"i",
+												)
+											}
+										>
+											+
+										</button>
+										<span>{bookingOptions.adult}</span>
+										<button
+											type="button"
+											disabled={bookingOptions.adult <= 0}
+											onClick={() =>
+												handleBookingOptions(
+													"adult",
+													"d",
+												)
+											}
+										>
+											-
+										</button>
+									</div>
+								</div>
+								<div className="o-flex x--between  y--center">
+									<span>Children</span>
+									<div className="count">
+										<button
+											type="button"
+											onClick={() =>
+												handleBookingOptions(
+													"children",
+													"i",
+												)
+											}
+										>
+											+
+										</button>
+										<span>{bookingOptions.children}</span>
+										<button
+											type="button"
+											disabled={bookingOptions.adult <= 0}
+											onClick={() =>
+												handleBookingOptions(
+													"children",
+													"d",
+												)
+											}
+										>
+											-
+										</button>
+									</div>
+								</div>
+								<div className="o-flex x--between  y--center">
+									<span>Room</span>
+									<div className="count">
+										<button
+											type="button"
+											onClick={() =>
+												handleBookingOptions(
+													"room",
+													"i",
+												)
+											}
+										>
+											+
+										</button>
+										<span>{bookingOptions.room}</span>
+										<button
+											type="button"
+											disabled={bookingOptions.adult <= 0}
+											onClick={() =>
+												handleBookingOptions(
+													"room",
+													"d",
+												)
+											}
+										>
+											-
+										</button>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-					<button type="button" className="c-btn">
+					<button type="button" className="search c-btn">
 						Search
 					</button>
 				</div>
-				<DateRangePicker
-					ranges={[selectionRange]}
-					onChange={handleSelect(selectionRange)}
-				/>
+			</div>
+
+			<div className="navcontainer">
+				<div className="datepicker">
+					{datePickerOpened && (
+						<DateRangePicker
+							onChange={(item) => setState([item.selection])}
+							showSelectionPreview
+							moveRangeOnFirstSelection={false}
+							months={2}
+							ranges={state}
+							direction="horizontal"
+							className="datepicker"
+						/>
+					)}
+				</div>
 			</div>
 		</div>
 	)
